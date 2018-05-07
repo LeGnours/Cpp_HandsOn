8123d50876590e012ab8ca974fb2ba8caea7d0c7
LeGnours <vincent.couvignou78@gmail.com>
2018-05-07 13:27:03 -0400

    Add ConcurrentAsyncQueue template class and unit tests for the constructor.

---
+++ ConcurrentAsyncQueueTests/ConcurrentAsyncQueueInit.hpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "pch.h"
+#include "../ConcurrentAsyncQueue/ConcurrentAsyncQueue.hpp"
+#include "ConcurrentAsyncQueueMock.hpp"
+
+using ::testing::TestWithParam;
+using ::testing::Bool;
+using ::testing::Values;
+using ::testing::Combine;
+
+class ConcurrentAsyncQueueInit : public TestWithParam<int>
+{
+public:
+	void SetUp() final
+	{
+		test_queue_ = new ConcurrentAsyncQueueMock<int>(GetParam());
+	}
+
+	void TearDown() final
+	{
+		delete test_queue_;
+		test_queue_ = nullptr;
+	}
+
+protected:
+	ConcurrentAsyncQueueMock<int> *test_queue_;
+};
+
+TEST_P(ConcurrentAsyncQueueInit, ConstructorInitWithNoParams)
+{
+	test_queue_ = new ConcurrentAsyncQueueMock<int>();
+
+	ASSERT_EQ(true, test_queue_->is_empty());
+	ASSERT_EQ(0, test_queue_->count());
+}
+
+TEST_P(ConcurrentAsyncQueueInit, ConstructorInitWithCapacityMaxSet)
+{
+	ASSERT_EQ(true, test_queue_->is_empty());
+	ASSERT_EQ(0, test_queue_->count());
+	ASSERT_EQ(GetParam(), test_queue_->get_capacity_changed());
+}
---
+++ ConcurrentAsyncQueueTests/ConcurrentAsyncQueueMock.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "gmock/gmock.h"
+#include "../ConcurrentAsyncQueue/ConcurrentAsyncQueue.hpp"
+
+template <typename T>
+class ConcurrentAsyncQueueMock : public ConcurrentAsyncQueue<T>
+{
+public:
+	using ConcurrentAsyncQueue<T>::ConcurrentAsyncQueue;
+
+	MOCK_METHOD0(get_queue_mutex, std::timed_mutex&());
+	MOCK_METHOD0(get_capacity_changed, std::condition_variable_any&());
+};
